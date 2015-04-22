41cd7fd4cecacf943ca42fa4a082d0c43bd4f0b4
Corinne Reitter <ceckel@gmail.com>
2015-04-21 22:30:32 -0700

    Fixed Product object being shown. Now shows product name.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,8 @@
 class Order < ActiveRecord::Base
   belongs_to :product
   validates :name, :street_address, :city, :state, :zipcode, :quantity, presence: true
+  
+  def product_name
+    return self.product.name
+  end
 end
